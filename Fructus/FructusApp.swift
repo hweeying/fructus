ddf89c6f9c232fe256a41a6d95adf74daa5ae25b
Jasmine <jasminehweying@gmail.com>
2023-05-10 21:03:47 +0800

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jasmine Lai Hweeying on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
